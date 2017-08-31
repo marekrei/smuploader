3525509a3c1882bbf9495fb9720054c870085196
Erik Selberg <erik@selberg.org>
2017-08-30 17:03:27 -0700

    turned test.py into clt; updated readme

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='smuploader',
-      version='0.1',
+      version='0.3',
       description='A SmugMug uploader and downloader. Uses SmugMug API v2.0',
       url='http://github.com/speedenator/smuploader',
       author='Marek Rei. Packaged by Erik Selberg',
       author_email='erik@selberg.org',
       license='MIT',
       packages=['smuploader'],
-      scripts=['bin/smuploader', 'bin/smdownloader'],
+      scripts=['bin/smuploader', 'bin/smdownloader', 'bin/smregister', 'bin/smregtest'],
       zip_safe=False)
